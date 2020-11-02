10f66c521ac9f1436d3a05a67b63b9ca98584474
Dominika Włodarczyk <dominikaa.99@gmail.com>
2020-11-02 22:05:37 +0100

    Add columns and cards structure

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import styles from './Hero.scss';
 import PropTypes from 'prop-types';
+import ReactHtmlParser from 'react-html-parser';
 
 const Hero = props => (
   <header className={styles.component}>
-    <h2 className={styles.title}>{props.titleText}</h2>
+    <h2 className={styles.title}>{ReactHtmlParser(props.titleText)}</h2>
     <img src={props.imageSrc} alt="space" className={styles.image}/>
   </header>
 );
 
 Hero.propTypes = {
-  titleText: PropTypes.node.isRequired,
+  titleText: PropTypes.node,
   imageSrc: PropTypes.string.isRequired,
 };
 
