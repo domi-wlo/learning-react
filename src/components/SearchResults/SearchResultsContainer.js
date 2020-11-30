53a993b4b84e9bdab6dbba48455d59d8208e7ef9
Dominika Włodarczyk <dominikaa.99@gmail.com>
2020-11-30 21:02:15 +0100

    Fix search results container

--- src/components/SearchResults/SearchResultsContainer.js
+++ src/components/SearchResults/SearchResultsContainer.js
@@ -1,19 +1,13 @@
 import {connect} from 'react-redux';
 import {getFilteredCards} from '../../redux/cardsRedux.js';
 import SearchResults from './SearchResults';
-// import createAction_changeSearchString from '../../redux/searchStringRedux.js';
+const mapStateToProps = (state, props) => {
 
-const mapStateToProps = (state) => {
-
-  const searchString = window.location.pathname.slice(8);
+  const searchString = props.match.params.searchString;
 
   return {
     cards: getFilteredCards(state, searchString),
   };
 };
-
-// const mapDispatchToProps = (dispatch) => ({
-//   changeSearchString: newSearchString => dispatch(createAction_changeSearchString(newSearchString)),
-// });
 
 export default connect(mapStateToProps)(SearchResults);
